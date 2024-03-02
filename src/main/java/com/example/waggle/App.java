4490e84d351a2eab3b154374af0e08518eb9241a
naviduNavoda <148700348+NavinduNavoda@users.noreply.github.com>
2024-03-02 21:30:32 +0530

    q

--- src/main/java/com/example/waggle/App.java
+++ src/main/java/com/example/waggle/App.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
-        //this is a cpmment
+        //this is a comment
 
     @Override
     public void start(Stage stage)  {
